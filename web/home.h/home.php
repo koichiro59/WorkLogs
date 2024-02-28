d0be18885d1bb88b6fa3a0ffc40ca6034e9d1d0d
koichiro59 <114040014+koichiro59@users.noreply.github.com>
2024-02-29 08:56:31 +0900

    Update home.php

--- web/home.h/home.php
+++ web/home.h/home.php
@@ -65,7 +65,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link  rel="stylesheet" href="{{ mix('./home.css') }}">
+    <link  rel="stylesheet" href="./home.css'">
     <title>WorkLogs</title>
 </head>
 <body>
