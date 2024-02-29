ea051359c1f2fe1f4817a9940f891f9f70902144
koichiro59 <114040014+koichiro59@users.noreply.github.com>
2024-02-29 09:17:08 +0900

    Update home.php

--- web/home.h/home.php
+++ web/home.h/home.php
@@ -65,7 +65,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="/home.css">
+    <link type="text/css" rel="stylesheet" href="https://worklogs-ed10984f34c2.herokuapp.com/home.h/home.pcss">
     <title>WorkLogs</title>
 </head>
 <body>
